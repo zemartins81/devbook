01eaca4a61b835dcebea59746c332bbb2ec9d4aa
José Carlos Martins <zemartins81@gmail.com>
2022-08-17 22:45:32 -0300

    Ajustando o neovim

--- src/controllers/usuarios.go
+++ src/controllers/usuarios.go
@@ -4,6 +4,7 @@ import (
 	"api/src/banco"
 	"api/src/modelos"
 	"api/src/repositorios"
+	"api/src/respostas"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
@@ -15,18 +16,23 @@ import (
 func CriarUsuario(w http.ResponseWriter, r *http.Request) {
 	corpoRequest, erro := ioutil.ReadAll(r.Body)
 	if erro != nil {
-		log.Fatal(erro)
+		respostas.Erro(w, http.StatusUnprocessableEntity, erro)
+		return
 	}
 
 	var usuario modelos.Usuario
 	if erro = json.Unmarshal(corpoRequest, &usuario); erro != nil {
-		log.Fatal(erro)
+		respostas.Erro(w, http.StatusBadRequest, erro)
+		return
+		}
 	}
 
 	db, erro := banco.Conectar()
 	if erro != nil {
-		log.Fatal(erro)
+		respostas.Er
 	}
+
+	defer db.Close()
 
 	repositorio := repositorios.NovoRepositorioDeUsuarios(db)
 	usuarioID, erro := repositorio.CriarUsuario(usuario)
