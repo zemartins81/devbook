a7dc9d116ca6037718cbb1ffac08509c448af442
José Carlos Martins <zemartins81@gmail.com>
2022-08-28 17:38:06 -0300

    Update User

--- src/controllers/usuarios.go
+++ src/controllers/usuarios.go
@@ -106,44 +106,44 @@ func BuscarUsuario(w http.ResponseWriter, r *http.Request) {
 // AtualizarUsuario atualiza os dados do usuário a partir do seu ID
 func AtualizarUsuario(w http.ResponseWriter, r *http.Request) {
 
-				parametros := mux.Vars(r)
-				usuarioID, erro := strconv.ParseUint(parametros["usuarioID", 10, 64])
-				if erro != nil {
-								respostas.Erro(w, http.StatusBadRequest, erro)
-								return
-				}
+	parametros := mux.Vars(r)
+	usuarioID, erro := strconv.ParseUint(parametros["usuarioID"], 10, 64)
+	if erro != nil {
+		respostas.Erro(w, http.StatusBadRequest, erro)
+		return
+	}
 
-				corpoRequisicao, erro := ioutil.ReadAll(r.Body)
-				if erro != nil {
-								respostas.Erro(w, http.StatusUnprocessableEntity, erro)
-								return
-				}
+	corpoRequisicao, erro := ioutil.ReadAll(r.Body)
+	if erro != nil {
+		respostas.Erro(w, http.StatusUnprocessableEntity, erro)
+		return
+	}
 
-				var usuario modelos.Usuario
-				if erro = json.Unmarshal(corpoRequisicao, &usuario); erro != nil {
-								respostas.Erro(w, http.StatusBadRequest, erro)
-								return
-				}
+	var usuario modelos.Usuario
+	if erro = json.Unmarshal(corpoRequisicao, &usuario); erro != nil {
+		respostas.Erro(w, http.StatusBadRequest, erro)
+		return
+	}
 
-				if erro = usuario.Preparar("edicao"); erro != nil {
-								respostas.Erro(w, http.StatusInternalServerError, erro)
-								return
-				}
+	if erro = usuario.Preparar("edicao"); erro != nil {
+		respostas.Erro(w, http.StatusInternalServerError, erro)
+		return
+	}
 
-				db, erro := banco.Conectar()
-				if erro != nil {
-								respostas.Erro(w, http.StatusInternalServerError, erro)
-								return
-				}
-				defer db.Close()
+	db, erro := banco.Conectar()
+	if erro != nil {
+		respostas.Erro(w, http.StatusInternalServerError, erro)
+		return
+	}
+	defer db.Close()
 
-				repositorio := repositorios.NovoRepositorioDeUsuarios(db)
-				if erro = repositorio.Atualizar(usuarioID, usuario); erro != nil {
-								respostas.Erro(w, http.StatusInternalServerError, erro)
-								return
-				}
+	repositorio := repositorios.NovoRepositorioDeUsuarios(db)
+	if erro = repositorio.Atualizar(usuarioID, usuario); erro != nil {
+		respostas.Erro(w, http.StatusInternalServerError, erro)
+		return
+	}
 
-				respostas.JSON(w, http.StatusNoContent, nil)
+	respostas.JSON(w, http.StatusNoContent, nil)
 }
 
 // DeletarUsuario remove um usuário do sistema
