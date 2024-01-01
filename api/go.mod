a37e65478cea5c01466dcae6e7091d1d4f73e3ac
José C. Martins <zemartins81@gmail.com>
2024-01-01 11:09:53 -0300

    Criando conexão com o DB

--- api/go.mod
+++ api/go.mod
@@ -3,6 +3,7 @@ module api
 go 1.21.5
 
 require (
+	github.com/go-sql-driver/mysql v1.7.1 // indirect
 	github.com/gorilla/mux v1.8.1 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
 )
---
+++ api/src/banco/banco.go
@@ -0,0 +1,24 @@
+package banco
+
+import (
+	"api/src/config"
+	"database/sql"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+// Conectar estabelece uma conexão com o banco de dados.
+// Retorna um ponteiro para o objeto sql.DB e um erro, se houver.
+func Conectar() (*sql.DB, error) {
+	db, erro := sql.Open("mysql", config.StringConexaoBanco)
+	if erro != nil {
+		return nil, erro
+	}
+
+	if erro = db.Ping(); erro != nil {
+		db.Close()
+		return nil, erro
+	}
+
+	return db, nil
+}
