238ec95dd556786dd21b4155adc95b55418f96d4
Jose C. Martins <zemartins81@gmail.com>
2024-02-26 15:28:48 -0300

    feat: adicionando tela de cadastro

---
+++ webapp/src/router/rotas/usuarios.go
@@ -0,0 +1,16 @@
+package rotas
+
+import (
+	"net/http"
+
+	"github.com/zemartins81/devbookWebApp/src/controllers"
+)
+
+var rotasUsuarios = []Rota{
+	{
+		URI:                "/criar-usuario",
+		Metodo:             http.MethodGet,
+		Funcao:             controllers.CarregarPaginaCriarUsuario,
+		RequerAutenticacao: false,
+	},
+}
